2dbae67a3f01955e982942341268f821ab23bb80
psgolopfruit-prog <psgolopfruit@gmail.com>
2025-12-08 12:01:05 +0500

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,27 +1,29 @@
 import { createPictures } from './data.js';
 import { openBigPicture } from './bigPicture.js';
+
 const pictureList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
 const pictures = createPictures();
-
 const similarListFragment = document.createDocumentFragment();
 
-createPictures.forEach((photo) => {
+pictures.forEach((photo) => {
   const { url, description, likes, comments } = photo;
   const picture = pictureTemplate.cloneNode(true);
+  
   picture.querySelector('.picture__img').src = url;
   picture.querySelector('.picture__img').alt = description;
   picture.querySelector('.picture__comments').textContent = comments.length;
   picture.querySelector('.picture__likes').textContent = likes;
-  similarListFragment.appendChild(picture);
   
-  picture.addEventListener('click', () => {
+  picture.addEventListener('click', (evt) => {
+    evt.preventDefault();
     openBigPicture(photo);
-
   });
+  
+  similarListFragment.appendChild(picture);
 });
 
 pictureList.appendChild(similarListFragment);
