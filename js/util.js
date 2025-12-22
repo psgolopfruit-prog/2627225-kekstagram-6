864da3c07e4d375fb1155672627e4d03c1e2d67c
psgolopfruit-prog <psgolopfruit@gmail.com>
2025-12-22 16:56:26 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -18,9 +18,34 @@ const createRandomNumbers = (min, max) => {
     return currentValue;
   };
 };
-
 const getRandomArrayElement = (elements) => elements[createRandomNumbers(0, elements.length - 1)()];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+const shuffleArray = (array) => {
+  for (let indexOne = array.length - 1; indexOne > 0; indexOne--) {
+    const indexTwo = Math.floor(Math.random() * (indexOne + 1));
+    [array[indexOne], array[indexTwo]] = [array[indexTwo], array[indexOne]];
+  }
+  return array;
+};
 
-export {getRandomInteger, createRandomNumbers, getRandomArrayElement, isEscapeKey};
+const showAlert = (message) => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '25px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = message;
+  document.body.append(messageAlert);
+};
+
+export { getRandomInteger, createRandomNumbers, getRandomArrayElement, isEscapeKey, debounce, shuffleArray, showAlert };
