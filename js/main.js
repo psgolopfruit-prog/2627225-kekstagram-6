d3dcef98181814ee433f8f3af76e73097db045e0
psgolopfruit-prog <psgolopfruit@gmail.com>
2025-12-22 16:16:26 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 import './pictures.js';
-import './form.js';
+import {loadData} from './fetch.js';
+import {renderPhotos} from './pictures.js';
+import './hashtags.js';
 import './slider-and-control.js';
 
-import './bigPicture.js';
-import './form.js';
+let photos;
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(data.slice());
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolue';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.textAlign = 'center';
+  messageAlert.textContent = 'Возникла ошибка при загрузке фотографий';
+  document.body.append(messageAlert);
+};
+
+loadData(onSuccess, onFail);
+
+export {photos};
