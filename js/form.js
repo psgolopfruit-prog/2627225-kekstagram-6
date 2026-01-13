da1bf8337c82dad889ec884bb8595641f328ee18
psgolopfruit-prog <psgolopfruit@gmail.com>
2026-01-13 22:03:30 +0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -86,6 +86,7 @@ const hideModal = () => {
   pristine.reset();
   resetSliderToNone();
   resetControlToStandart();
+  submitButton.disabled = false;
   formOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
